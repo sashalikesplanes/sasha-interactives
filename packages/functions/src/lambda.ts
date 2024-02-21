97b224933c310beb4f406c41b8550c32259fd66d
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2024-02-22 00:22:30 +0100

    feat: test hello world lambda

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,8 +1,9 @@
 import { ApiHandler } from "sst/node/api";
 
 export const handler = ApiHandler(async (_evt) => {
+  console.log("Hello from the API handler");
   return {
     statusCode: 200,
-    body: `Hello world. The time is ${new Date().toISOString()}`,
+    body: `Hello sasha!!!. The time is ${new Date().toISOString()}`,
   };
 });
