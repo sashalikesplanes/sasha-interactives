821f8be15f63fc3b423f601f5e10968e1a4ba3a2
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2024-02-22 02:20:50 +0100

    feat: add resize and save

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,27 +1,60 @@
 import { ApiHandler } from "sst/node/api";
 import OpenAI from "openai";
 import { Config } from "sst/node/config";
+import mqtt from "mqtt";
+import { S3, S3Client, UploadPartCommand } from "@aws-sdk/client-s3";
+import { Bucket } from "sst/node/bucket";
+import crypto from "crypto";
+import sharp from "sharp";
 
-const openai = new OpenAI({
+const openaiClient = new OpenAI({
   apiKey: Config.OPENAI_SECRET_KEY
 });
 
+const mqttClient = await mqtt.connectAsync(`mqtt://${Config.HIVEMQ_URL}`, {
+  username: Config.HIVEMQ_USERNAME,
+  protocol: 'mqtts',
+  port: 8883,
+  password: Config.HIVEMQ_PASSWORD,
+});
+
+const s3Client = new S3({ region: "eu-west-1" });
+
 export const handler = ApiHandler(async (_evt) => {
-  const res = await openai.images.generate({
+  const res = await openaiClient.images.generate({
     n: 1,
-    size: "1024x1024",
-    prompt: "An abstract representation of your interpretation of the feeling of 'shit'. Design it so that it would look good scaled down to a 64x64 pixel display.",
-    model: "dall-e-3",
+    size: "256x256",
+    prompt: "An abstract representation of your interpretation of the feeling of sadness. Design it so that it would look good scaled down to a 64x64 pixel display. MAKE IT PIXELATED",
+    model: "dall-e-2",
     style: "vivid",
     quality: "standard",
-    response_format: "url",
+    response_format: "b64_json",
   });
+  const b64 = res.data[0].b64_json;
+  if (!b64) throw new Error("no image data returned by openai");
 
-  console.log(res.data.map((e) => e.url));
+  const b64Buffer = Buffer.from(b64, "base64url");
+  const resizedB64 = await sharp(b64Buffer).resize(64).toBuffer();
+  const bucketName = Bucket["generated-images"].bucketName;
+  const key = crypto.randomUUID();
+  const url = `https://${bucketName}.s3.amazonaws.com/${key}`;
+  await s3Client.putObject({
+    Bucket: bucketName,
+    Key: key,
+    ACL: "public-read",
+    Body: resizedB64,
+    ContentType: "image/webp"
+  })
+
+  console.log(`Image uploaded: ${url}`);
+
+  mqttClient.publish("test", "test");
+  console.log("Published to mqtt")
+
 
   console.log("Hello from the API handler");
   return {
     statusCode: 200,
-    body: `Hello sasha!!!. The time is ${new Date().toISOString()}`,
+    body: `Hello sasha!!!. The time is ${new Date().toISOString()}, the url is : ${url}`,
   };
 });
--- stacks/stack.ts
+++ stacks/stack.ts
@@ -1,5 +1,8 @@
 import { StackContext, Api, EventBus, Bucket, Config } from "sst/constructs";
 
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+
 export function Functions({ stack }: StackContext) {
   const OPENAI_SECRET_KEY = new Config.Secret(stack, "OPENAI_SECRET_KEY");
 
@@ -7,7 +10,13 @@ export function Functions({ stack }: StackContext) {
   const HIVEMQ_USERNAME = new Config.Parameter(stack, "HIVEMQ_USERNAME", { value: "sasha" });
   const HIVEMQ_URL = new Config.Parameter(stack, "HIVEMQ_URL", { value: "f1f452b5379d437ca5f74369e7279ff0.s1.eu.hivemq.cloud" });
 
-  const bucket = new Bucket(stack, "generated-images");
+  const sharpLayer = new lambda.LayerVersion(stack, "sharpLayer", {
+    code: lambda.Code.fromAsset("layers/sharp"),
+  });
+
+  const bucket = new Bucket(stack, "generated-images", {
+    blockPublicACLs: false,
+  });
 
   const api = new Api(stack, "functions", {
     routes: {
@@ -15,6 +24,16 @@ export function Functions({ stack }: StackContext) {
     },
     defaults: {
       function: {
+        nodejs: {
+          esbuild: {
+            external: ["sharp"]
+          }
+        },
+        layers: [
+          // Sharp layer downloaded from https://github.com/Umkus/lambda-layer-sharp/releases and uploaded with their instructions
+          "arn:aws:lambda:us-west-2:856334325530:layer:sharp",
+        ],
+        timeout: 900,
         bind: [OPENAI_SECRET_KEY, HIVEMQ_USERNAME, HIVEMQ_PASSWORD, HIVEMQ_URL, bucket]
       }
     }
